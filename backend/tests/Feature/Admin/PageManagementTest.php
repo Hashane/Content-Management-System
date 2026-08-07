7a64b2ed1977e1af3047307c1a84b8137bfd668a
Hashane <jhashane@gmail.com>
2026-08-07 21:48:49 +0300

    chore: basic setup for test cases.

--- backend/tests/Feature/Admin/PageManagementTest.php
+++ backend/tests/Feature/Admin/PageManagementTest.php
@@ -2,19 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PageManagementTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setup(): void
+    {
+        parent::setUp();
+        $this->seed([PermissionSeeder::class, RoleSeeder::class]);
+    }
+
+    private function admin(): User
+    {
+        $user = User::factory()->create();
+        $user->assignRole('admin');
+
+        return $user;
+    }
+
+    private function moderator(): User
+    {
+        $user = User::factory()->create();
+        $user->assignRole('moderator');
+
+        return $user;
     }
 }
