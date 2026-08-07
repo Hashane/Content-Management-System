5c5ba9972a9e1fd40a5bdc88dc08d2ca1a4ff4fa
Hashane <jhashane@gmail.com>
2026-08-07 19:13:55 +0300

    refactor: pint

--- backend/app/Models/Page.php
+++ backend/app/Models/Page.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use App\Enums\PageStatus;
 use App\Observers\PageObserver;
+use Database\Factories\PageFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
-
 #[ObservedBy([PageObserver::class])]
 class Page extends Model
 {
-    /** @use HasFactory<\Database\Factories\PageFactory> */
+    /** @use HasFactory<PageFactory> */
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
@@ -55,7 +55,6 @@ class Page extends Model
 
     /**
      * Published pages that are due to be visible
-     * 
      */
     public function scopePublishedAndDue(Builder $query): Builder
     {
