eb94d6c6d7788ebae83566803ccff469ecea09e7
Hashane <jhashane@gmail.com>
2026-08-07 15:23:30 +0300

    implementation: response macros created.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Policies\PrivilegePolicy;
 use App\Policies\RolePolicy;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -26,5 +28,21 @@ class AppServiceProvider extends ServiceProvider
     {
         Gate::policy(Role::class, RolePolicy::class);
         Gate::policy(Permission::class, PrivilegePolicy::class);
+
+        Response::macro('success', function (mixed $data = null, string $message = 'Success', int $code = 200): JsonResponse {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data,
+            ], $code);
+        });
+
+        Response::macro('error', function (string $message, int $code = 400, mixed $errors = null): JsonResponse {
+            return response()->json([
+                'success' => false,
+                'message' => $message,
+                ...($errors ? ['errors' => $errors] : []),
+            ], $code);
+        });
     }
 }
