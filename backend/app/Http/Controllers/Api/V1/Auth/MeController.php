94d60e344496b8903c965e926eb5ec9e196a0a15
Hashane <jhashane@gmail.com>
2026-08-07 18:13:03 +0300

    bug-fix: use proper response macro.

--- backend/app/Http/Controllers/Api/V1/Auth/MeController.php
+++ backend/app/Http/Controllers/Api/V1/Auth/MeController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
-    public function __invoke(Request $request): UserResource
+    public function __invoke(Request $request): JsonResponse
     {
-        return new UserResource($request->user());
+        return response()->success(new UserResource($request->user()));
     }
 }
