b07157b7dced1deac7eb768bd889f640cb853306
Hashane <jhashane@gmail.com>
2026-08-08 20:16:12 +0300

    config: vite config to forward req to backend.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cms.test',
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: 'localhost',
+      },
+      '/sanctum': {
+        target: 'https://cms.test',
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: 'localhost',
+      },
+    },
+  },
 })
