35cbbe8cc0d6365584a86c0f49304a5783cfb522
Hashane <jhashane@gmail.com>
2026-08-08 19:55:14 +0300

    chore: auth foundation
    - added withCredentials: true to accept & attach Sanctum auth cookie

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -4,6 +4,16 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    Accept: 'application/json',
+  },
+});
+
+export const authClient = axios.create({
+  baseURL: '/',
+  withCredentials: true,
+  // withXSRFToken: true,
   headers: {
     Accept: 'application/json',
   },
